937f92ac3ded46ac69aeed907f4bfc9394219cbb
dave doolin <david.doolin@gmail.com>
2017-07-11 07:26:33 -0700

    exercise 1-3 on pattern matching implemented
    pinning operator, seems interesting. Will be even
    more interesting to see how it works in practice.

--- test/exercise_1_3_test.exs
+++ test/exercise_1_3_test.exs
@@ -7,7 +7,46 @@ defmodule Exercise13Test do
   # I have a record for each of the situations presented in
   # the exercises.
   describe "Exercise:Pattern Matching-3" do
-    @tag :skip
-    test '[a, b, a] = [1, 2, 3] raises match error'
+    test '[a, b, a] = [1, 2, 3] raises match error' do
+      a = 2
+      list = [1, 2, 3]
+      assert_raise MatchError, fn ->
+        [a, _b, a] = list
+      end
+    end
+
+    test "[a, b, a] = [1, 1, 2] raises match error" do
+      a = 2
+      list = [1, 1, 2]
+      assert_raise MatchError, fn ->
+        [a, _b, a] = list
+      end
+    end
+
+    test "reassignment works" do
+      a = 2
+      a = 1
+      assert a == 1
+    end
+
+    test "^a works" do
+      a = 2
+      ^a = 2
+      assert a == 2
+    end
+
+    test "^a = 1 raises" do
+      a = 2
+      assert_raise MatchError, fn ->
+        ^a = 1
+      end
+    end
+
+    test "^a = 2 - a raises" do
+      a = 2
+      assert_raise MatchError, fn ->
+        ^a = 2 - a
+      end
+    end
   end
 end
