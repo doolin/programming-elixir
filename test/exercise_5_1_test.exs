1913bfd99704ff5cbb70bb59275c03bfd4f2d4b9
dave doolin <david.doolin@gmail.com>
2017-07-15 19:01:01 -0700

    finished exercise 5.1
    This turned out to be pretty simple as long as I
    defined the function locally in the test. Might be
    worth revisiting to see how to define a method for
    producing such functions.

--- test/exercise_5_1_test.exs
+++ test/exercise_5_1_test.exs
@@ -8,5 +8,19 @@ defmodule Exercise51Test do
       end
       assert list_concat.([:a, :b], [:c, :d]) == [:a, :b, :c, :d]
     end
+
+    test "create sum function" do
+      sum = fn (a, b, c) ->
+        a + b + c
+      end
+      assert sum.(1, 2, 3) == 6
+    end
+
+    test "pair_tuple_to_list function" do
+      pair_tuple_to_list = fn (tuple) ->
+        tuple |> Tuple.to_list
+      end
+      assert pair_tuple_to_list.({1, 2}) == [1, 2]
+    end
   end
 end
