329b2c9aff8601c9217f681e721e645c688cf3b1
dave doolin <david.doolin@gmail.com>
2017-07-17 09:08:52 -0700

    fizzbuzz elixir style, pretty cool

--- test/exercise_5_2_test.exs
+++ test/exercise_5_2_test.exs
@@ -5,16 +5,20 @@ defmodule Exercise52Test do
     "FizzBuzz"
   end
 
+  def fizzbuzz(a, 0, c) do
+    "Buzz"
+  end
+
   def fizzbuzz(0, b, c) do
     "Fizz"
   end
 
-  def fizzbuzz(1, 0, c) do
-    "Buzz"
-  end
-
   def fizzbuzz(a, b, c) do
     c
+  end
+
+  def fizzbuzz(n) do
+    fizzbuzz(rem(n, 3), rem(n, 5), n)
   end
 
   describe "Exercise:Anonymous Functions-2" do
@@ -23,6 +27,12 @@ defmodule Exercise52Test do
       assert fizzbuzz(0, 1, 3) == "Fizz"
       assert fizzbuzz(1, 0, 3) == "Buzz"
       assert fizzbuzz(1, 2, 3) == 3
+    end
+
+    test "FizzBuzz with single argument" do
+      assert fizzbuzz(15) == "FizzBuzz"
+      assert fizzbuzz(5) == "Buzz"
+      assert fizzbuzz(3) == "Fizz"
     end
   end
 end
