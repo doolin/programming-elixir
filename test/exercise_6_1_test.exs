0d1919c8ac94675e9acbdccbe8ae89a8e075d44e
dave doolin <david.doolin@gmail.com>
2017-07-21 14:56:09 -0700

    p. 49, quadruple function
    pipe the arg through double twice. cool

--- test/exercise_6_1_test.exs
+++ test/exercise_6_1_test.exs
@@ -5,6 +5,8 @@ defmodule Times do
   end
 
   def triple(n), do: n * 3
+
+  def quadruple(n), do: n |> double |> double
 end
 
 defmodule Exercise61Test do
@@ -26,6 +28,13 @@ defmodule Exercise61Test do
   describe "add triple function" do
     test "triple" do
       assert Times.triple(2) == 6
+    end
+  end
+
+  # exercise 6.3, all of these run together
+  describe "add quadruple funtion" do
+    test "quadruple" do
+      assert Times.quadruple(2) == 8
     end
   end
 end
