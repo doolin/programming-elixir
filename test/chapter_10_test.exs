33113a9aa0730d778b2d97e2f27f0ec336183f1a
dave doolin <david.doolin@gmail.com>
2019-01-17 04:04:24 -0800

    p. 111 back on the road again with Elixir
    One of my main techniques is unit testing examples
    in the book, which is what this commit does.

--- test/chapter_10_test.exs
+++ test/chapter_10_test.exs
@@ -6,4 +6,9 @@ defmodule Chapter10Test do
            |> Enum.map(&(&1*&1))
     assert list == [1, 4, 9, 16, 25]
   end
+
+  test 'empty comprehension' do
+    result = Enum.into 1..5, []
+    assert result == [1, 2, 3, 4, 5]
+  end
 end
