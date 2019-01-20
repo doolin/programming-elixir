5e52e70a57cb77065200bb6c5b0e3c6fb0bbd1e0
dave doolin <david.doolin@gmail.com>
2019-01-19 17:39:40 -0800

    assign from comprehension

--- test/chapter_10_test.exs
+++ test/chapter_10_test.exs
@@ -11,4 +11,15 @@ defmodule Chapter10Test do
     result = Enum.into 1..5, []
     assert result == [1, 2, 3, 4, 5]
   end
+
+  test 'append to comprehension' do
+    result = Enum.into 1..3, [101]
+    assert result == [101, 1, 2, 3]
+  end
+
+  test 'comprehension for squaring' do
+    foo = for x <- [1, 2, 3, 4, 5], do: x * x
+    assert x = [1, 4, 9, 16, 25]
+    assert foo = [1, 4, 9, 16, 25]
+  end
 end
