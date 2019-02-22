e952e543aa4dcc64dc3bc994095066eaca7ea042
dave doolin <david.doolin@gmail.com>
2019-02-22 04:14:35 -0800

    another lame commit, work is seriously loading me up
    I'm stuck on something today at work, which is taking
    more time than I want it to.

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -2,4 +2,12 @@ defmodule Fizzbuzz do
   def upto(n) when n > 0, do: _downto(n, [])
 
   defp _downto(0, result), do: result
+
+  defp _downto(current, result) do
+    next_answer = 
+      cond do
+        rem(current, 3) == 0 and rem(current, 5) == 0 ->
+          "fizzbuzz"
+      end
+  end
 end
