fdff9df8a0eadddb08cac8a47361b86e099939cb
dave doolin <david.doolin@gmail.com>
2019-02-24 14:28:32 -0800

    fixed symbol error, was a typo

--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -1,8 +1,8 @@
-defmodule FizzBuzzTest do
+defmodule FizzbuzzTest do
   use ExUnit.Case
 
   test "fizzes" do
-    actual = FizzBuzz.upto(3)
+    actual = Fizzbuzz.upto(3)
     expected = [1, 2, "fizz"]
     assert expected == actual
   end
