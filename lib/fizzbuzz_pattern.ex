802df28e2fff263f41802f8b9337ac360196d7c5
dave doolin <david.doolin@gmail.com>
2019-02-28 03:26:56 -0800

    crawling along

--- lib/fizzbuzz_pattern.ex
+++ lib/fizzbuzz_pattern.ex
@@ -1,3 +1,5 @@
 defmodule FizzbuzzPattern do
   def upto(n) when n > 0, do: 1..n |> Enum.map(&fizzbuzz/1)
+
+  def fizzbuzz(n), do: _fizzword(n, rem(n, 3), rem(n, 5))
 end
--- test/fizzbuzz_pattern_test.exs
+++ test/fizzbuzz_pattern_test.exs
@@ -2,8 +2,8 @@ defmodule FizzbuzzPattern do
   use ExUnit.Case
 
   describe "fizzbuzz with pattern matching" do
-    test "for 15" do
-      assert 1 == 0
+    test "for 3" do
+      FizzbuzzPattern.upto(3)
     end
   end
 end
