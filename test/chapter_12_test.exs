20b3a48200d082ed9175ef78e6fbc48b7f8fd9e8
dave doolin <david.doolin@gmail.com>
2019-03-03 07:46:35 -0800

    revisiting parts of chapter 12

--- test/chapter_12_test.exs
+++ test/chapter_12_test.exs
@@ -1,7 +1,13 @@
 defmodule Chapter12Test do
   use ExUnit.Case
 
-  test "start chapter 12" do
-    assert 1 == 1
+  test "syntax for if-else statement" do
+    result =
+      if 1 == 1 do
+        "OK"
+      else
+        "error"
+      end
+    assert result == "OK"
   end
 end
