846ecb74dbcebab4079ef95d501dbaaa6b403a10
dave doolin <david.doolin@gmail.com>
2019-03-30 09:07:46 -0700

    link2 is working

--- lib/link2.exs
+++ lib/link2.exs
@@ -5,4 +5,16 @@ defmodule Link2 do
     sleep 500
     exit(:boom)
   end
+
+  def run do
+    spawn_link(Link2, :sad_function, [])
+    receive do
+      msg ->
+        IO.puts "MESSAGE RECEIVED: #{inspect msg}"
+    after 1000 ->
+      IO.puts "Nothing happened as fas as I am concerned"
+    end
+  end
 end
+
+Link2.run
