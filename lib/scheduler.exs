982ac3995586223a5684130b26a93b989223c0d5
dave doolin <david.doolin@gmail.com>
2019-04-07 12:57:04 -0700

    p. 213 schedule

---
+++ lib/scheduler.exs
@@ -0,0 +1,7 @@
+defmodule Scheduler do
+  def run(num_processes, module, func, to_calculate) do
+    (1..num_processes)
+    |> Enum.map(fn(_) -> spawn(module, func, [self()]) end)
+    |> schedule_processes(to_calculate, [])
+  end
+end
