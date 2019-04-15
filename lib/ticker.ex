9a02e10756b859b7c1ac8fcba4d69216dde8f196
dave doolin <david.doolin@gmail.com>
2019-04-15 04:19:39 -0700

    p. 224 continue with ticker

--- lib/ticker.ex
+++ lib/ticker.ex
@@ -7,4 +7,8 @@ defmodule Ticker do
     pid = spawn(__MODULE__, :generator, [[]]])
     :global.register_name(@name, pid)
   end
+
+  def register(client_pid) do
+    send :global.whereis_name(@name), { :register, client_pid }
+  end
 end
