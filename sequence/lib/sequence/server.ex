b4cff3642ee705d5f4ff3c0dbbac085f9838f420
dave doolin <david.doolin@gmail.com>
2019-04-19 06:52:18 -0700

    p. 231 server finished

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -4,4 +4,8 @@ defmodule Sequence.Server do
   def init(initial_number) do
     { :ok, initial_number }
   end
+
+  def handle_call(:next_number, _from, current_number) do
+    {:reply, current_number, current_number + 1}
+  end
 end
