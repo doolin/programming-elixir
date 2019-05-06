8048ddbbe9fa7bcd83b48c31d9d1f2c3c12c9025
dave doolin <david.doolin@gmail.com>
2019-05-06 03:56:25 -0700

    duper results punched in

--- duper/lib/duper/results.ex
+++ duper/lib/duper/results.ex
@@ -43,4 +43,10 @@ defmodule Duper.Results do
       results
     }
   end
+
+  defp hashes_with_more_than_one_path(results) do
+    results
+    |> Enum.filter(fn { _hash, paths } -> length(paths) > 1 end)
+    |> Enum.map(&elem(&1, 1))
+  end
 end
