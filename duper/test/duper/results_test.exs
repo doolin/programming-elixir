5fae902ccdeed2963e9e2f8ed70ad57ab23449e1
dave doolin <david.doolin@gmail.com>
2019-05-09 03:50:11 -0700

    finished punching in test. doesn't run

--- duper/test/duper/results_test.exs
+++ duper/test/duper/results_test.exs
@@ -6,9 +6,15 @@ defmodule Duper.ResultsTest do
     Results.add_hash_for("path1", 123)
     Results.add_hash_for("path2", 456)
     Results.add_hash_for("path3", 123)
+    Results.add_hash_for("path4", 789)
+    Results.add_hash_for("path5", 456)
+    Results.add_hash_for("path6", 999)
 
     duplicates = Results.find_duplicates()
 
     assert length(duplicates) == 1
+
+    assert ~w{path3 path1} in duplicates
+    assert ~w{path5 path2} in duplicates
   end
 end
