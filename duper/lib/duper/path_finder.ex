f422697a525e560c6626416598ab366fa4ce1f1e
dave doolin <david.doolin@gmail.com>
2019-05-14 03:54:30 -0700

    p. 265 another tiny commit
    People watching closely will discern I'm spending more
    time on memory work than working on these small dailies.
    I probably won't remember that looking back on this chain
    of commits years from now, will probably just decide I
    was being lazy. But it's really true: I'm putting a lot
    of effort into SRS memory.

--- duper/lib/duper/path_finder.ex
+++ duper/lib/duper/path_finder.ex
@@ -6,4 +6,12 @@ defmodule PathFinder do
   def start_link(root) do
     GenServer.start_link(__MODULE__, root, name: @me)
   end
+
+  def next_path() do
+    GenServer.call(@me, :next_path)
+  end
+
+  def init(path) do
+    DirWalker.start_link(path)
+  end
 end
