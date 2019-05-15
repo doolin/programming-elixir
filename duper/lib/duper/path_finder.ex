4ac5ac3e34c9213716b1584b579825991a267695
dave doolin <david.doolin@gmail.com>
2019-05-15 04:11:38 -0700

    pathfinder

--- duper/lib/duper/path_finder.ex
+++ duper/lib/duper/path_finder.ex
@@ -14,4 +14,12 @@ defmodule PathFinder do
   def init(path) do
     DirWalker.start_link(path)
   end
+
+  def handle_call(:next_path, _from, dir_walker) do
+    path = case DirWalker.next(dir_walker) do
+            [ path ] -> path
+            other  -> other
+           end
+    { :reply, path, dir_walker }
+  end
 end
