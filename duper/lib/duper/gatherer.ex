b1a869d75eb677e540097e16c718ff924520c491
dave doolin <david.doolin@gmail.com>
2019-05-19 04:27:04 -0700

    more gatherer

--- duper/lib/duper/gatherer.ex
+++ duper/lib/duper/gatherer.ex
@@ -3,6 +3,8 @@ defmodule Duper.Gatherer do
 
   @me Gatherer
 
+  # api
+
   def start_link(worker_count) do
     GenServer.start_link(__MODULE__, worker_count, name: @me)
   end
@@ -13,5 +15,11 @@ defmodule Duper.Gatherer do
 
   def result(path, hash) do
     GenServer.cast(@me, { :result, path, hash })
+  end
+
+  # server
+
+  def init(worker_count) do
+    { :ok, worker_count }
   end
 end
