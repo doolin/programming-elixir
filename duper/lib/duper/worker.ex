d1baa32329068cbf8172d7e61f3c5ae4a9e917b6
dave doolin <david.doolin@gmail.com>
2019-05-29 04:11:05 -0700

    worker init

--- duper/lib/duper/worker.ex
+++ duper/lib/duper/worker.ex
@@ -4,4 +4,9 @@ defmodule Duper.Worker do
   def start_link(_) do
     GenServer.start_link(__MODULE__, :no_args)
   end
+
+  def init(:no_args) do
+    Process.send_after(self(), :do_one_file, 0)
+    { :ok, nil }
+  end
 end
