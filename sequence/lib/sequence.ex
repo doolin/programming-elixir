3abea8c9632dd4ed3634174cbac70d8fbdd06372
dave doolin <david.doolin@gmail.com>
2019-06-11 03:51:13 -0700

    p. 243 rewrite sequence as server
    This is reworking the Sequence material in Chapter 17.

--- sequence/lib/sequence.ex
+++ sequence/lib/sequence.ex
@@ -1,18 +1,3 @@
 defmodule Sequence do
-  @moduledoc """
-  Documentation for Sequence.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Sequence.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @server Sequence.Server
 end
