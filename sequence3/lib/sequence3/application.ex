2c5986309d5fa6959618bb4bf3efb6207503db87
dave doolin <david.doolin@gmail.com>
2019-06-14 04:10:39 -0700

    p. 248 Chapter 18: possible reread, more detail
    This is another example of the Sequence server. I know
    I have worked ahead of this chapter because I see further
    page numbers down in the commit log, but the material is
    not as familiar as I want it to be, so Chapter 18 is
    being redone as `sequence3`.

---
+++ sequence3/lib/sequence3/application.ex
@@ -0,0 +1,20 @@
+defmodule Sequence3.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Sequence3.Worker.start_link(arg)
+      # {Sequence3.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Sequence3.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
